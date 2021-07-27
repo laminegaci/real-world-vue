9c3d0748d14e416ea270104f4b9c7aad39d07150
laminegaci <mohamed61lamine@gmail.com>
2021-07-27 16:10:40 +0100

    push for delete

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Contact from "../views/Contact.vue";
+import User from '@/views/User.vue';
 
 Vue.use(VueRouter);
 
@@ -18,6 +20,16 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact
+  },
+  {
+    path: "/user/:username",
+    name: "User",
+    component: User
   }
 ];
 
---
+++ src/views/User.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">
+    <h1>This is {{ $route.params.username }} page</h1>
+  </div>
+</template>
